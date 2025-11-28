dc9edccf6944614cbc9dc85764c3d056ebae8407
williamiracolwill <williamiracolwill@gmail.com>
2025-11-28 02:43:16 -0700

    added functionality to display up to 10 movies

--- index.js
+++ index.js
@@ -12,31 +12,41 @@ movieSearchForm.addEventListener('submit', (e) => {
 })
 
 async function getMovies(title) {
-    const res = await fetch(`http://www.omdbapi.com/?t=${title}&type=movie&apikey=467c506d`)
-    const moviedata = await res.json()
-    console.log(moviedata)
-    movieList.innerHTML = getMovieHtml(moviedata)
+    const res = await fetch(`http://www.omdbapi.com/?s=${title}&type=movie&apikey=467c506d`)
+    const movieData = await res.json()
+    // console.log(movieData.Search)
+    let moviesArr = []
+    for (let movie of movieData.Search) {
+        const res = await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&type=movie&apikey=467c506d`)
+        const movieObj = await res.json()
+        moviesArr.push(movieObj)
+    }
+    movieList.innerHTML = getMovieHtml(moviesArr)
 }
 
-function getMovieHtml(obj) {
-    const {Title, Genre, Runtime, Plot, imdbRating, imdbID, Poster} = obj
-    return `
-    <div class="movie-item">
-        <img class="movie-poster" src=${Poster} alt="${Title} movie poster">
-        <div class="movie-info">
-            <div class="main-info">
-                <h2 class="movie-title">${Title}</h2>
-                <img src="assets/star-icon.png">
-                <p class="movie-rating">${imdbRating}</p>
+function getMovieHtml(arr) {
+    return arr.map(movie => {
+        const {Title, Genre, Runtime, Plot, imdbRating, imdbID, Poster} = movie
+        return `
+        <div class="movie-item">
+            <img class="movie-poster" src=${Poster} alt="${Title} movie poster">
+            <div class="movie-info">
+                <div class="main-info">
+                    <h2 class="movie-title">${Title}</h2>
+                    <img src="assets/star-icon.png">
+                    <p class="movie-rating">${imdbRating}</p>
+                </div>
+                <div class="sub-info">
+                    <p>${Runtime}</p>
+                    <p>${Genre}</p>
+                    <button class="add-movie-btn"><img src="assets/add-icon.png"> Watchlist</button>
+                </div>
+                <p class="movie-plot">${Plot}</p>
             </div>
-            <div class="sub-info">
-                <p>${Runtime}</p>
-                <p>${Genre}</p>
-                <button class="add-movie-btn"><img src="assets/add-icon.png"> Watchlist</button>
-            </div>
-            <p class="movie-plot">${Plot}</p>
-        </div>
-    </div>`
+        </div>`
+    })
+
+    
 }
 
 
