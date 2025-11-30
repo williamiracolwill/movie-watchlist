90a4aa879e315226458a0c125cdf8df19e5fc767
williamiracolwill <williamiracolwill@gmail.com>
2025-11-30 12:17:09 -0700

    Items in localStorage now display in watchlist page

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const movieSearchForm = document.getElementById("movie-search-form")
 const movieList = document.getElementById("movie-list")
 const initialState = document.getElementById("initial-state")
+const movieWatchlist = document.getElementById("movie-watchlist")
+
+const moviesArr = []
+let savedMovies = []
+
+const watchlistLocalStorage = JSON.parse(localStorage.getItem("savedMovies"))
+
+if (watchlistLocalStorage) {
+    savedMovies = watchlistLocalStorage
+    renderWatchlist(savedMovies)
+}
 
 
 movieSearchForm.addEventListener('submit', (e) => {
@@ -18,14 +29,11 @@ movieList.addEventListener('click', e => {
     }
 })
 
-const moviesArr = []
-const savedMovies = []
-
 function addMovie(movieId) {
     const targetMovieObj = moviesArr.filter(movie => movie.imdbID === movieId)[0]
     savedMovies.push(targetMovieObj)
     localStorage.setItem("savedMovies", JSON.stringify(savedMovies))
-    console.log(JSON.parse(localStorage.getItem("savedMovies")))
+    renderWatchlist()
 }
 
 async function getMovies(title) {
@@ -75,11 +83,36 @@ function getMovieHtml(arr) {
                 <p class="movie-plot">${Plot}</p>
             </div>
         </div>`
-    }).join('')
-
-    
+    }).join('')  
 }
 
+function getWatchlistHtml(arr) {
+    return arr.map(movie => {
+        const {Title, Genre, Runtime, Plot, imdbRating, imdbID, Poster} = movie
+        return `
+        <div class="movie-item">
+            <img class="movie-poster" src=${Poster} alt="${Title} movie poster">
+            <div class="movie-info">
+                <div class="main-info">
+                    <h2 class="movie-title">${Title}</h2>
+                    <img src="assets/star-icon.png">
+                    <p class="movie-rating">"${imdbRating}"</p>
+                </div>
+                <div class="sub-info">
+                    <p>${Runtime}</p>
+                    <p>${Genre}</p>
+                    <button class="add-movie-btn" data-movie="${imdbID}"><img src="assets/remove-icon.png" alt="remove button" data-movie="${imdbID}" /> Watchlist</button>
+                </div>
+                <p class="movie-plot">${Plot}</p>
+            </div>
+        </div>`
+    }).join('')
+}
 
+function renderWatchlist(arr) {
+    movieWatchlist.innerHTML = getWatchlistHtml(arr)
+}
+
+renderWatchlist()
 // make watchlist html page and set up default state
 // handle add to watchlist functionality..
